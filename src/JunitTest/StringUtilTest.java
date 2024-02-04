d2175b585443ce61e3694e02ad9066620c43d530
Yannan Zhang <zhangyannan199744@outlook.com>
2024-02-04 18:42:06 +0200

    2/4 Junit Test

---
+++ src/JunitTest/StringUtilTest.java
@@ -0,0 +1,13 @@
+package JunitTest;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for StringUtil
+ */
+public class StringUtilTest {
+    @Test
+    public void testPrintNumber(){
+        StringUtil.printNumber("admin");
+    }
+}
